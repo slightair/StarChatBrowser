fc7466601d90672ed88f10a0354e3ba44c646f88
Tomohiro Moro <arksutite@gmail.com>
2012-06-11 03:28:03 +0900

    SCBUserStreamClient で CFNetworkを使うようにし、再接続がきちんと働くようにした

--- StarChatBrowser/SCBUserStreamClient.m
+++ StarChatBrowser/SCBUserStreamClient.m
@@ -8,22 +8,38 @@
 
 #import "SCBUserStreamClient.h"
 
+#define kClientBufferSize 2048
+
+// CFNetwork CFReadStreamClientCallBack
+void readHttpStreamCallBack(CFReadStreamRef stream, CFStreamEventType eventType, void *clientCallBackInfo);
+
 @interface SCBUserStreamClient ()
+
+- (void)releaseReadStream;
 
 @property (strong) NSString *username;
 @property (strong) SBJsonStreamParserAdapter *streamParserAdapter;
 @property (strong) SBJsonStreamParser *streamParser;
 @property          SCBUserStreamClientConnectionStatus connectionStatus;
 
+// AFHTTPClient
+@property (readwrite, nonatomic, retain) NSMutableDictionary *defaultHeaders;
+
 @end
 
 @implementation SCBUserStreamClient
+{
+    CFReadStreamRef _readStreamRef;
+}
 
 @synthesize username = _username;
 @synthesize streamParserAdapter = _streamParserAdapter;
 @synthesize streamParser = _streamParser;
 @synthesize delegate = _delegate;
 @synthesize connectionStatus = _connectionStatus;
+
+// AFHTTPClient
+@synthesize defaultHeaders = _defaultHeaders;
 
 - (id)initWithBaseURL:(NSURL *)url username:(NSString *)username
 {
@@ -46,14 +62,36 @@
 
 - (void)start
 {
-    NSMutableURLRequest *request = [self requestWithMethod:@"GET" path:[NSString stringWithFormat:@"/users/%@/stream", self.username] parameters:nil];
-    
     self.connectionStatus = kSCBUserStreamClientConnectionStatusConnecting;
     if ([self.delegate respondsToSelector:@selector(userStreamClientWillConnect:)]) {
         [self.delegate userStreamClientWillConnect:self];
     }
     
-    [NSURLConnection connectionWithRequest:request delegate:self];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"/users/%@/stream", self.username] relativeToURL:self.baseURL];
+    CFURLRef urlRef = CFURLCreateWithString(kCFAllocatorDefault, (__bridge CFStringRef)[url absoluteString], NULL);
+    
+    CFHTTPMessageRef messageRef = CFHTTPMessageCreateRequest(kCFAllocatorDefault, CFSTR("GET"), urlRef, kCFHTTPVersion1_1);
+    for (NSString *headerFieldName in self.defaultHeaders) {
+        CFHTTPMessageSetHeaderFieldValue(messageRef, (__bridge CFStringRef)headerFieldName, (__bridge CFStringRef)[self defaultValueForHeader:headerFieldName]);
+    }
+    CFHTTPMessageSetHeaderFieldValue(messageRef, CFSTR("Accept"), CFSTR("application/json"));
+    
+    _readStreamRef = CFReadStreamCreateForHTTPRequest(kCFAllocatorDefault, messageRef);
+    CFStreamClientContext contextRef = {0, (__bridge void *)self, NULL, NULL, NULL};
+    
+    if (CFReadStreamSetClient(_readStreamRef,
+                              (kCFStreamEventOpenCompleted  |
+                               kCFStreamEventHasBytesAvailable |
+                               kCFStreamEventEndEncountered |
+                               kCFStreamEventErrorOccurred),
+                              &readHttpStreamCallBack,
+                              &contextRef)) {
+        CFReadStreamScheduleWithRunLoop(_readStreamRef, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+    }
+    CFReadStreamOpen(_readStreamRef);
+    
+    CFRelease(messageRef);
+    CFRelease(urlRef);
 }
 
 - (void)reconnect
@@ -61,35 +99,20 @@
     [self start];
 }
 
-#pragma mark -
-#pragma mark NSURLConnectionDelegate Methods
-
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    self.connectionStatus = kSCBUserStreamClientConnectionStatusConnected;
-    if ([self.delegate respondsToSelector:@selector(userStreamClientDidConnected:)]) {
-        [self.delegate userStreamClientDidConnected:self];
-    }
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+- (void)releaseReadStream
 {
-    [self.streamParser parse:data];
+    if (_readStreamRef) {
+        CFRelease(_readStreamRef);
+    }
+    _readStreamRef = NULL;
 }
 
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    self.connectionStatus = kSCBUserStreamClientConnectionStatusDisconnected;
-    if ([self.delegate respondsToSelector:@selector(userStreamClient:didFailWithError:)]) {
-        [self.delegate userStreamClient:self didFailWithError:error];
+- (void)dealloc
+{
+    if (_readStreamRef) {
+        CFRelease(_readStreamRef);
     }
 }
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    self.connectionStatus = kSCBUserStreamClientConnectionStatusDisconnected;
-    if ([self.delegate respondsToSelector:@selector(userStreamClientDidDisconnected:)]) {
-        [self.delegate userStreamClientDidDisconnected:self];
-    }
-}
-
 
 #pragma mark -
 #pragma mark SBJsonStreamParserAdapterDelegate Methods
@@ -106,3 +129,65 @@
 }
 
 @end
+
+// CFNetwork CFReadStreamClientCallBack
+void readHttpStreamCallBack(CFReadStreamRef stream, CFStreamEventType eventType, void *clientCallBackInfo) {
+    SCBUserStreamClient *client = (__bridge SCBUserStreamClient *)clientCallBackInfo;
+    
+    switch (eventType) {
+        case kCFStreamEventOpenCompleted:
+        {
+            client.connectionStatus = kSCBUserStreamClientConnectionStatusConnected;
+            if ([client.delegate respondsToSelector:@selector(userStreamClientDidConnected:)]) {
+                [client.delegate userStreamClientDidConnected:client];
+            }
+            
+            break;
+        }
+        case kCFStreamEventHasBytesAvailable:
+        {
+            UInt8 buffer[kClientBufferSize];
+            CFIndex bytesRead = CFReadStreamRead(stream, buffer, sizeof(buffer));
+            
+            if (bytesRead > 0) {
+                NSData *data = [NSData dataWithBytes:buffer length:bytesRead];
+                [client.streamParser parse:data];
+            }
+            
+            break;
+        }
+        case kCFStreamEventEndEncountered:
+        {
+            client.connectionStatus = kSCBUserStreamClientConnectionStatusDisconnected;
+            if ([client.delegate respondsToSelector:@selector(userStreamClientDidDisconnected:)]) {
+                [client.delegate userStreamClientDidDisconnected:client];
+            }
+            
+            CFReadStreamUnscheduleFromRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+            CFReadStreamClose(stream);
+            [client releaseReadStream];
+            
+            break;
+        }
+        case kCFStreamEventErrorOccurred:
+        {
+            CFErrorRef errorRef = CFReadStreamCopyError(stream);
+            CFDictionaryRef userInfoRef = CFErrorCopyUserInfo(errorRef);
+            NSError *error = [[NSError alloc] initWithDomain:(__bridge NSString *)CFErrorGetDomain(errorRef) code:CFErrorGetCode(errorRef) userInfo:(__bridge NSDictionary *)userInfoRef];
+            
+            CFRelease(userInfoRef);
+            CFRelease(errorRef);
+            
+            client.connectionStatus = kSCBUserStreamClientConnectionStatusFailed;
+            if ([client.delegate respondsToSelector:@selector(userStreamClient:didFailWithError:)]) {
+                [client.delegate userStreamClient:client didFailWithError:error];
+            }
+            
+            CFReadStreamUnscheduleFromRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopCommonModes);
+            CFReadStreamClose(stream);
+            [client releaseReadStream];
+            
+            break;
+        }
+    }
+}
